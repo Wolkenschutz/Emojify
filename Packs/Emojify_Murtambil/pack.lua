d9d57cd63c1f461f9f050d131894ae9ac2838f77
Shantalya <selina.ruesch@gmail.com>
2025-10-15 03:19:31 +0200

    Added Murtambil Pack

---
+++ Packs/Emojify_Murtambil/pack.lua
@@ -0,0 +1,58 @@
+local addonName = ...;
+
+local pack = Emojify:RegisterPack(addonName);
+
+pack:AddEmoji("murtamAww", 64);
+pack:AddEmoji("murtamBonk", 64);
+pack:AddEmoji("murtamBuff", 64);
+pack:AddEmoji("murtamComfy", 64);
+pack:AddEmoji("murtamCool", 64);
+pack:AddEmoji("murtamCredit", 64);
+pack:AddEmoji("murtamCross", 64);
+pack:AddEmoji("murtamCry", 64);
+pack:AddEmoji("murtamDerp", 64);
+pack:AddEmoji("murtamEvil", 64);
+pack:AddEmoji("murtamFine", 64);
+pack:AddEmoji("murtamHehe", 64);
+pack:AddEmoji("murtamKiss", 64);
+pack:AddEmoji("murtamLOL", 64);
+pack:AddEmoji("murtamLove", 64);
+pack:AddEmoji("murtamLurk", 64);
+pack:AddEmoji("murtamNote", 64);
+pack:AddEmoji("murtamPat", 64);
+pack:AddEmoji("murtamPog", 64);
+pack:AddEmoji("murtamRaid", 64);
+pack:AddEmoji("murtamSip", 64);
+pack:AddEmoji("murtamSmug", 64);
+pack:AddEmoji("murtamSuffer", 64);
+pack:AddEmoji("murtamThink", 64);
+pack:AddEmoji("murtamTrash", 64);
+pack:AddEmoji("murtamUwU", 64);
+pack:AddEmoji("murtamWaiting", 64);
+pack:AddAnimatedEmoji("murtamBlelele", 6, 64)
+    :SetDefaultDelay(50);
+
+pack:AddAnimatedEmoji("murtamDance", 10, 64)
+    :SetDefaultDelay(70);
+
+pack:AddAnimatedEmoji("murtamGun", 10, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("murtamHamsti", 10, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("murtamRee", 25, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("murtamStinky", 50, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("murtamTail", 60, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("murtamWave", 16, 64)
+    :SetDefaultDelay(50);
+
+pack:AddAnimatedEmoji("murtamWheeze", 25, 64)
+    :SetDefaultDelay(60);
+
