e609260a4efc5f1a454f06ffbe3621e9a2a9b3d9
Shantalya <selina.ruesch@gmail.com>
2025-10-15 03:19:51 +0200

    Added Schwowi Pack

---
+++ Packs/Emojify_Schwowi/pack.lua
@@ -0,0 +1,43 @@
+local addonName = ...;
+
+local pack = Emojify:RegisterPack(addonName);
+
+pack:AddEmoji("schwowBonk", 64);
+pack:AddEmoji("schwowBuffed", 64);
+pack:AddEmoji("schwowBurn", 64);
+pack:AddEmoji("schwowCool", 64);
+pack:AddEmoji("schwowHeart", 64);
+pack:AddEmoji("schwowHello", 64);
+pack:AddEmoji("schwowHi", 64);
+pack:AddEmoji("schwowHype", 64);
+pack:AddEmoji("schwowLOL", 64);
+pack:AddEmoji("schwowLove", 64);
+pack:AddEmoji("schwowMoney", 64);
+pack:AddEmoji("schwowOw", 64);
+pack:AddEmoji("schwowPalm", 64);
+pack:AddEmoji("schwowRip", 64);
+pack:AddEmoji("schwowSleep", 64);
+pack:AddEmoji("schwowSorry", 64);
+pack:AddEmoji("schwowWhat", 64);
+pack:AddEmoji("schwowWork", 64);
+pack:AddAnimatedEmoji("schwowAfk", 18, 64)
+    :SetDefaultDelay(60)
+    :SetFrameDelay(17, 500);
+
+pack:AddAnimatedEmoji("schwowCat", 12, 64)
+    :SetDefaultDelay(80)
+    :SetFrameDelay(11, 800);
+
+pack:AddAnimatedEmoji("schwowLook", 6, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("schwowMvp", 26, 64)
+    :SetDefaultDelay(40)
+    :SetFrameDelay(25, 1000);
+
+pack:AddAnimatedEmoji("schwowRaid", 20, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("schwowRoll", 20, 64)
+    :SetDefaultDelay(70);
+
