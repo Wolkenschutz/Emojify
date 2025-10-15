a7ad8f15a7e2c9f09d5a462a1d5ea3573df24e5d
Shantalya <selina.ruesch@gmail.com>
2025-10-15 03:46:54 +0200

    Added Official WoW Dracthyr Emojis

---
+++ Packs/Emojify_Dracthyr/pack.lua
@@ -0,0 +1,47 @@
+local addonName = ...;
+
+local pack = Emojify:RegisterPack(addonName);
+
+pack:AddEmoji("dracthyrA", 128, 64);
+pack:AddEmoji("dracthyrNo", 128, 64);
+pack:AddEmoji("dracthyrComfyBlack", 64);
+pack:AddEmoji("dracthyrComfyBlue", 64);
+pack:AddEmoji("dracthyrComfyBronze", 64);
+pack:AddEmoji("dracthyrComfyGreen", 64);
+pack:AddEmoji("dracthyrComfyRed", 64);
+pack:AddEmoji("dracthyrComfySip", 64);
+pack:AddEmoji("dracthyrCrylaugh", 64);
+pack:AddEmoji("dracthyrHeart", 64);
+pack:AddEmoji("dracthyrLulmao", 64);
+pack:AddEmoji("dracthyrShrug", 64);
+pack:AddEmoji("dracthyrSweat", 64);
+pack:AddEmoji("dracthyrTea", 64);
+pack:AddEmoji("dracthyrUwu", 64);
+
+pack:AddAnimatedEmoji("dracthyrBlobDance", 37, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("dracthyrCry", 3, 64)
+    :SetDefaultDelay(0);
+
+pack:AddAnimatedEmoji("dracthyrHehe", 8, 64)
+    :SetDefaultDelay(30);
+
+pack:AddAnimatedEmoji("dracthyrLove", 2, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("dracthyrNervous", 4, 64)
+    :SetDefaultDelay(70);
+
+pack:AddAnimatedEmoji("dracthyrNod", 5, 64)
+    :SetDefaultDelay(100)
+    :SetFrameDelay(0, 160)
+    :SetFrameDelay(2, 130)
+    :SetFrameDelay(4, 60);
+
+pack:AddAnimatedEmoji("dracthyrUwahh", 3, 64)
+    :SetDefaultDelay(30);
+
+pack:AddAnimatedEmoji("dracthyrYay", 4, 64)
+    :SetDefaultDelay(50);
+
