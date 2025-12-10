cc44c594fa8711ebe75d7695db06b1a3175b94d1
Shantalya <selina.ruesch@gmail.com>
2025-12-10 23:15:58 +0100

    Update Schwowi Pack

--- Packs/Emojify_Schwowi/pack.lua
+++ Packs/Emojify_Schwowi/pack.lua
@@ -10,25 +10,43 @@ pack:AddEmoji("schwowHeart", 64);
 pack:AddEmoji("schwowHello", 64);
 pack:AddEmoji("schwowHi", 64);
 pack:AddEmoji("schwowHype", 64);
-pack:AddEmoji("schwowLOL", 64);
 pack:AddEmoji("schwowLove", 64);
-pack:AddEmoji("schwowMoney", 64);
+pack:AddEmoji("schwowLul", 64);
 pack:AddEmoji("schwowOw", 64);
 pack:AddEmoji("schwowPalm", 64);
 pack:AddEmoji("schwowRip", 64);
 pack:AddEmoji("schwowSleep", 64);
 pack:AddEmoji("schwowSorry", 64);
+pack:AddEmoji("schwowTimed", 64);
 pack:AddEmoji("schwowWhat", 64);
 pack:AddEmoji("schwowWork", 64);
 pack:AddAnimatedEmoji("schwowAfk", 18, 64)
     :SetDefaultDelay(60)
     :SetFrameDelay(17, 500);
 
+pack:AddAnimatedEmoji("schwowBoom", 15, 64)
+    :SetDefaultDelay(100)
+    :SetFrameDelay(2, 300)
+    :SetFrameDelay(4, 500);
+
 pack:AddAnimatedEmoji("schwowCat", 12, 64)
     :SetDefaultDelay(80)
     :SetFrameDelay(11, 800);
 
+pack:AddAnimatedEmoji("schwowDj", 8, 64)
+    :SetDefaultDelay(100)
+    :SetFrameDelay(2, 300)
+    :SetFrameDelay(4, 400)
+    :SetFrameDelay(6, 400)
+    :SetFrameDelay(7, 500);
+
 pack:AddAnimatedEmoji("schwowLook", 6, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("schwowMagni", 25, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("schwowMount", 6, 64)
     :SetDefaultDelay(40);
 
 pack:AddAnimatedEmoji("schwowMvp", 26, 64)
