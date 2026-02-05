ad65616d81632f9c79dc20c1e6e0719b692bacfc
Shantalya <selina.ruesch@gmail.com>
2026-02-05 19:53:55 +0100

    Update xJeevy pack

--- Packs/Emojify_xJeevy/pack.lua
+++ Packs/Emojify_xJeevy/pack.lua
@@ -4,8 +4,7 @@ local pack = Emojify:RegisterPack(addonName);
 
 pack:AddEmoji("xjeevyAlo", 64);
 pack:AddEmoji("xjeevyAngry", 64);
-pack:AddEmoji("xjeevyChirstmascheer", 64);
-pack:AddEmoji("xjeevyChristmasMug", 64);
+pack:AddEmoji("xjeevyDerp", 64);
 pack:AddEmoji("xjeevyDOIT", 64);
 pack:AddEmoji("xjeevyDuh", 64);
 pack:AddEmoji("xjeevyGiggle", 64);
@@ -22,6 +21,8 @@ pack:AddEmoji("xjeevyKinSlurp", 64);
 pack:AddEmoji("xjeevyMad", 64);
 pack:AddEmoji("xjeevyMana", 64);
 pack:AddEmoji("xjeevyNEED", 64);
+pack:AddEmoji("xjeevyNo", 64);
+pack:AddEmoji("xjeevyNote", 64);
 pack:AddEmoji("xjeevyPatpat", 64);
 pack:AddEmoji("xjeevyPoggers", 64);
 pack:AddEmoji("xjeevyPwease", 64);
@@ -30,6 +31,7 @@ pack:AddEmoji("xjeevyShiney", 64);
 pack:AddEmoji("xjeevySmelly", 64);
 pack:AddEmoji("xjeevyStare", 64);
 pack:AddEmoji("xjeevyTake", 64);
+pack:AddEmoji("xjeevyThis", 64);
 pack:AddAnimatedEmoji("xjeevyAllgood", 60, 64)
     :SetDefaultDelay(30)
     :SetFrameDelay(1, 40)
@@ -63,9 +65,47 @@ pack:AddAnimatedEmoji("xjeevyBell", 30, 64)
 pack:AddAnimatedEmoji("xjeevyBoink", 35, 64)
     :SetDefaultDelay(20);
 
-pack:AddAnimatedEmoji("xjeevyClaus", 58, 64)
-    :SetDefaultDelay(40)
-    :SetFrameDelay(0, 120);
+pack:AddAnimatedEmoji("xjeevyBroke", 64, 64)
+    :SetDefaultDelay(30)
+    :SetFrameDelay(1, 60)
+    :SetFrameDelay(4, 60)
+    :SetFrameDelay(8, 60)
+    :SetFrameDelay(11, 60)
+    :SetFrameDelay(14, 60)
+    :SetFrameDelay(18, 60)
+    :SetFrameDelay(21, 60)
+    :SetFrameDelay(24, 60)
+    :SetFrameDelay(27, 60)
+    :SetFrameDelay(31, 60)
+    :SetFrameDelay(34, 60)
+    :SetFrameDelay(37, 60)
+    :SetFrameDelay(41, 60)
+    :SetFrameDelay(44, 60)
+    :SetFrameDelay(47, 60)
+    :SetFrameDelay(50, 60)
+    :SetFrameDelay(54, 60)
+    :SetFrameDelay(57, 60)
+    :SetFrameDelay(60, 60)
+    :SetFrameDelay(63, 60);
+
+pack:AddAnimatedEmoji("xjeevyCheer", 60, 64)
+    :SetDefaultDelay(20)
+    :SetFrameDelay(2, 30)
+    :SetFrameDelay(6, 30)
+    :SetFrameDelay(11, 30)
+    :SetFrameDelay(15, 30)
+    :SetFrameDelay(20, 30)
+    :SetFrameDelay(24, 30)
+    :SetFrameDelay(29, 30)
+    :SetFrameDelay(33, 30)
+    :SetFrameDelay(38, 30)
+    :SetFrameDelay(42, 30)
+    :SetFrameDelay(47, 30)
+    :SetFrameDelay(51, 30)
+    :SetFrameDelay(56, 30);
+
+pack:AddAnimatedEmoji("xjeevyCold", 71, 64)
+    :SetDefaultDelay(30);
 
 pack:AddAnimatedEmoji("xjeevyDance", 55, 64)
     :SetDefaultDelay(30);
@@ -74,6 +114,9 @@ pack:AddAnimatedEmoji("xjeevyEmotional", 9, 64)
     :SetDefaultDelay(120);
 
 pack:AddAnimatedEmoji("xjeevyEYO", 40, 64)
+    :SetDefaultDelay(30);
+
+pack:AddAnimatedEmoji("xjeevyFinger", 25, 64)
     :SetDefaultDelay(30);
 
 pack:AddAnimatedEmoji("xjeevyFlappy", 25, 64)
@@ -89,37 +132,11 @@ pack:AddAnimatedEmoji("xjeevyHerzen", 25, 64)
 pack:AddAnimatedEmoji("xjeevyHunt", 58, 64)
     :SetDefaultDelay(50);
 
-pack:AddAnimatedEmoji("xjeevyLurk", 59, 64)
-    :SetDefaultDelay(40)
-    :SetFrameDelay(1, 30)
-    :SetFrameDelay(3, 30)
-    :SetFrameDelay(5, 30)
-    :SetFrameDelay(8, 30)
-    :SetFrameDelay(10, 30)
-    :SetFrameDelay(12, 30)
-    :SetFrameDelay(15, 30)
-    :SetFrameDelay(17, 30)
-    :SetFrameDelay(19, 30)
-    :SetFrameDelay(22, 30)
-    :SetFrameDelay(24, 30)
-    :SetFrameDelay(26, 30)
-    :SetFrameDelay(29, 30)
-    :SetFrameDelay(31, 30)
-    :SetFrameDelay(33, 30)
-    :SetFrameDelay(36, 30)
-    :SetFrameDelay(38, 30)
-    :SetFrameDelay(40, 30)
-    :SetFrameDelay(43, 30)
-    :SetFrameDelay(45, 30)
-    :SetFrameDelay(47, 30)
-    :SetFrameDelay(50, 30)
-    :SetFrameDelay(52, 30)
-    :SetFrameDelay(54, 30)
-    :SetFrameDelay(57, 30)
-    :SetFrameDelay(58, 30);
+pack:AddAnimatedEmoji("xjeevyHydrate", 20, 64)
+    :SetDefaultDelay(40);
 
-pack:AddAnimatedEmoji("xjeevyPadoru", 4, 64)
-    :SetDefaultDelay(33);
+pack:AddAnimatedEmoji("xjeevyMegaphone", 61, 64)
+    :SetDefaultDelay(30);
 
 pack:AddAnimatedEmoji("xjeevyPanic", 43, 64)
     :SetDefaultDelay(30);
@@ -172,45 +189,4 @@ pack:AddAnimatedEmoji("xjeevySmart", 54, 64)
 
 pack:AddAnimatedEmoji("xjeevySpray", 48, 64)
     :SetDefaultDelay(40);
-
-pack:AddAnimatedEmoji("xjeevyWintercold", 71, 64)
-    :SetDefaultDelay(30);
-
-pack:AddAnimatedEmoji("xjeevyWintergift", 59, 64)
-    :SetDefaultDelay(30)
-    :SetFrameDelay(1, 40)
-    :SetFrameDelay(4, 40)
-    :SetFrameDelay(7, 40)
-    :SetFrameDelay(10, 40)
-    :SetFrameDelay(13, 40)
-    :SetFrameDelay(16, 40)
-    :SetFrameDelay(19, 40)
-    :SetFrameDelay(22, 40)
-    :SetFrameDelay(25, 40)
-    :SetFrameDelay(28, 40)
-    :SetFrameDelay(31, 40)
-    :SetFrameDelay(34, 40)
-    :SetFrameDelay(37, 40)
-    :SetFrameDelay(40, 40)
-    :SetFrameDelay(43, 40)
-    :SetFrameDelay(46, 40)
-    :SetFrameDelay(49, 40)
-    :SetFrameDelay(52, 40)
-    :SetFrameDelay(55, 40);
-
-pack:AddAnimatedEmoji("xjeevyWinterhype", 60, 64)
-    :SetDefaultDelay(20)
-    :SetFrameDelay(2, 30)
-    :SetFrameDelay(6, 30)
-    :SetFrameDelay(11, 30)
-    :SetFrameDelay(15, 30)
-    :SetFrameDelay(20, 30)
-    :SetFrameDelay(24, 30)
-    :SetFrameDelay(29, 30)
-    :SetFrameDelay(33, 30)
-    :SetFrameDelay(38, 30)
-    :SetFrameDelay(42, 30)
-    :SetFrameDelay(47, 30)
-    :SetFrameDelay(51, 30)
-    :SetFrameDelay(56, 30);
 
