9de32cef31b0ffb2701e2fe8153a2d8a46d67a04
Shantalya <selina.ruesch@gmail.com>
2026-07-24 02:13:52 +0200

    New Twitch emotes from Chromie! Welcome <3

---
+++ Packs/Emojify_Chromie/pack.lua
@@ -0,0 +1,126 @@
+local addonName = ...;
+
+local pack = Emojify:RegisterPack(addonName);
+
+pack:AddEmoji("chromiedeAngry", 64);
+pack:AddEmoji("chromiedeBRO", 64);
+pack:AddEmoji("chromiedeChromielove", 64);
+pack:AddEmoji("chromiedeCrab", 64);
+pack:AddEmoji("chromiedeDragonheart", 64);
+pack:AddEmoji("chromiedeDrink", 64);
+pack:AddEmoji("chromiedeFrost", 64);
+pack:AddEmoji("chromiedeGasm", 64);
+pack:AddEmoji("chromiedeGoil", 64);
+pack:AddEmoji("chromiedeHaltstop", 64);
+pack:AddEmoji("chromiedeHay", 64);
+pack:AddEmoji("chromiedeHm", 64);
+pack:AddEmoji("chromiedeHoly", 64);
+pack:AddEmoji("chromiedeHuhn", 64);
+pack:AddEmoji("chromiedeHype", 64);
+pack:AddEmoji("chromiedeIon", 64);
+pack:AddEmoji("chromiedeKappa", 64);
+pack:AddEmoji("chromiedeLach", 64);
+pack:AddEmoji("chromiedeLel", 64);
+pack:AddEmoji("chromiedeLore", 64);
+pack:AddEmoji("chromiedeLul", 64);
+pack:AddEmoji("chromiedeMA", 64);
+pack:AddEmoji("chromiedeMadness", 64);
+pack:AddEmoji("chromiedeMurloc", 64);
+pack:AddEmoji("chromiedeOmg", 64);
+pack:AddEmoji("chromiedeRage", 64);
+pack:AddEmoji("chromiedeSaddish", 64);
+pack:AddEmoji("chromiedeSkeptisch", 64);
+pack:AddEmoji("chromiedeSmirk", 64);
+pack:AddEmoji("chromiedeSpider", 64);
+pack:AddEmoji("chromiedeSweat", 64);
+pack:AddEmoji("chromiedeTeam", 64);
+pack:AddEmoji("chromiedeUwu", 64);
+pack:AddEmoji("chromiedeWahnsinn", 64);
+pack:AddEmoji("chromiedeWave", 64);
+pack:AddEmoji("chromiedeWell", 64);
+pack:AddEmoji("chromiedeWhat", 64);
+pack:AddEmoji("chromiedeXalatath", 64);
+pack:AddEmoji("chromiedeXalheart", 64);
+pack:AddEmoji("chromiedeXalol", 64);
+pack:AddEmoji("chromiedeYO", 64);
+pack:AddAnimatedEmoji("chromiedeAngst", 4, 64)
+    :SetDefaultDelay(70);
+
+pack:AddAnimatedEmoji("chromiedeChicken", 9, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("chromiedeCry", 3, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("chromiedeDeathrave", 22, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("chromiedeDracthyr", 37, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("chromiedeGg", 50, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("chromiedeHearts", 10, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("chromiedeHehe", 8, 64)
+    :SetDefaultDelay(30);
+
+pack:AddAnimatedEmoji("chromiedeHi", 50, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("chromiedeHuhu", 4, 64)
+    :SetDefaultDelay(50);
+
+pack:AddAnimatedEmoji("chromiedeIonyes", 12, 64)
+    :SetDefaultDelay(80)
+    :SetFrameDelay(11, 800);
+
+pack:AddAnimatedEmoji("chromiedeJa", 5, 64)
+    :SetDefaultDelay(100)
+    :SetFrameDelay(0, 160)
+    :SetFrameDelay(2, 130)
+    :SetFrameDelay(4, 60);
+
+pack:AddAnimatedEmoji("chromiedeKekw", 21, 64)
+    :SetDefaultDelay(20);
+
+pack:AddAnimatedEmoji("chromiedeLove", 57, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("chromiedeLoveroll", 8, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("chromiedeLulul", 9, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("chromiedeNeeeee", 9, 64)
+    :SetDefaultDelay(60);
+
+pack:AddAnimatedEmoji("chromiedeNo", 3, 64)
+    :SetDefaultDelay(30);
+
+pack:AddAnimatedEmoji("chromiedePedro", 60, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("chromiedePrimusroll", 8, 64)
+    :SetDefaultDelay(100);
+
+pack:AddAnimatedEmoji("chromiedeRad", 20, 64)
+    :SetDefaultDelay(70);
+
+pack:AddAnimatedEmoji("chromiedeShake", 25, 64)
+    :SetDefaultDelay(40);
+
+pack:AddAnimatedEmoji("chromiedeSylvie", 27, 64)
+    :SetDefaultDelay(40)
+    :SetFrameDelay(0, 500)
+    :SetFrameDelay(26, 500);
+
+pack:AddAnimatedEmoji("chromiedeXaladance", 12, 64)
+    :SetDefaultDelay(80);
+
+pack:AddAnimatedEmoji("chromiedeO7", 50, 64)
+    :SetDefaultDelay(40);
+
--- README.md
+++ README.md
@@ -55,6 +55,7 @@ Special thanks to [Arukori](https://twitch.tv/arukori) for her contributions to 
 - **Twitch: [artemirgaming](https://twitch.tv/artemirgaming)** - Full emote collection
 - **Twitch: [BroStudios_WoW](https://twitch.tv/BroStudios_WoW)** - Full emote collection
 - **Twitch: [YusakiKirin](https://twitch.tv/YusakiKirin)** - Full emote collection
+- **Twitch: [Chromie](https://twitch.tv/Chromie)** - Full emote collection
 - **BetterTTV** - Trending community emotes
 - **Dracthyr** - Official World of Warcraft Dracthyr emotes
 
